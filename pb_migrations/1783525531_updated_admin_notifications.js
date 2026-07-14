f0b61fc3d208e494f282f2b0f84451aa5eaa44ae
Fixofy User <user@example.com>
2026-07-14 21:38:48 +0530

    render

--- pb_migrations/1783525531_updated_admin_notifications.js
+++ pb_migrations/1783525531_updated_admin_notifications.js
@@ -2,120 +2,60 @@
 migrate((app) => {
   const collection = app.findCollectionByNameOrId("pbc_4269230398")
 
-  // remove field
-  collection.fields.removeById("autodate1000000084")
+  if (!collection.fields.hasFieldWithName("type")) {
+    collection.fields.addAt(1, new Field({
+      "autogeneratePattern": "",
+      "hidden": false,
+      "id": "text1000000079",
+      "max": 0,
+      "min": 0,
+      "name": "type",
+      "pattern": "",
+      "presentable": false,
+      "primaryKey": false,
+      "required": true,
+      "system": false,
+      "type": "text"
+    }))
+  }
 
-  // update field
-  collection.fields.addAt(1, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "text1000000079",
-    "max": 0,
-    "min": 0,
-    "name": "type",
-    "pattern": "",
-    "presentable": false,
-    "primaryKey": false,
-    "required": true,
-    "system": false,
-    "type": "text"
-  }))
+  if (!collection.fields.hasFieldWithName("title")) {
+    collection.fields.addAt(2, new Field({
+      "autogeneratePattern": "",
+      "hidden": false,
+      "id": "text1000000080",
+      "max": 0,
+      "min": 0,
+      "name": "title",
+      "pattern": "",
+      "presentable": false,
+      "primaryKey": false,
+      "required": true,
+      "system": false,
+      "type": "text"
+    }))
+  }
 
-  // update field
-  collection.fields.addAt(2, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "text1000000080",
-    "max": 0,
-    "min": 0,
-    "name": "title",
-    "pattern": "",
-    "presentable": false,
-    "primaryKey": false,
-    "required": true,
-    "system": false,
-    "type": "text"
-  }))
-
-  // update field
-  collection.fields.addAt(3, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "text1000000081",
-    "max": 0,
-    "min": 0,
-    "name": "body",
-    "pattern": "",
-    "presentable": false,
-    "primaryKey": false,
-    "required": true,
-    "system": false,
-    "type": "text"
-  }))
+  if (!collection.fields.hasFieldWithName("body")) {
+    collection.fields.addAt(3, new Field({
+      "autogeneratePattern": "",
+      "hidden": false,
+      "id": "text1000000081",
+      "max": 0,
+      "min": 0,
+      "name": "body",
+      "pattern": "",
+      "presentable": false,
+      "primaryKey": false,
+      "required": true,
+      "system": false,
+      "type": "text"
+    }))
+  }
 
   return app.save(collection)
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_4269230398")
-
-  // add field
-  collection.fields.addAt(6, new Field({
-    "hidden": false,
-    "id": "autodate1000000084",
-    "name": "createdAt",
-    "onCreate": true,
-    "onUpdate": false,
-    "presentable": false,
-    "system": false,
-    "type": "autodate"
-  }))
-
-  // update field
-  collection.fields.addAt(1, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "text1000000079",
-    "max": 0,
-    "min": 0,
-    "name": "type",
-    "pattern": "",
-    "presentable": false,
-    "primaryKey": false,
-    "required": false,
-    "system": false,
-    "type": "text"
-  }))
-
-  // update field
-  collection.fields.addAt(2, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "text1000000080",
-    "max": 0,
-    "min": 0,
-    "name": "title",
-    "pattern": "",
-    "presentable": false,
-    "primaryKey": false,
-    "required": false,
-    "system": false,
-    "type": "text"
-  }))
-
-  // update field
-  collection.fields.addAt(3, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "text1000000081",
-    "max": 0,
-    "min": 0,
-    "name": "body",
-    "pattern": "",
-    "presentable": false,
-    "primaryKey": false,
-    "required": false,
-    "system": false,
-    "type": "text"
-  }))
 
   return app.save(collection)
 })
--- pb_migrations/1783526454_updated_admin_notifications.js
+++ pb_migrations/1783526454_updated_admin_notifications.js
@@ -2,39 +2,35 @@
 migrate((app) => {
   const collection = app.findCollectionByNameOrId("pbc_4269230398")
 
-  // add field
-  collection.fields.addAt(1, new Field({
-    "hidden": false,
-    "id": "autodate2990389176",
-    "name": "created",
-    "onCreate": true,
-    "onUpdate": false,
-    "presentable": false,
-    "system": false,
-    "type": "autodate"
-  }))
+  if (!collection.fields.hasFieldWithName("created")) {
+    collection.fields.addAt(1, new Field({
+      "hidden": false,
+      "id": "autodate2990389176",
+      "name": "created",
+      "onCreate": true,
+      "onUpdate": false,
+      "presentable": false,
+      "system": false,
+      "type": "autodate"
+    }))
+  }
 
-  // add field
-  collection.fields.addAt(2, new Field({
-    "hidden": false,
-    "id": "autodate3332085495",
-    "name": "updated",
-    "onCreate": true,
-    "onUpdate": true,
-    "presentable": false,
-    "system": false,
-    "type": "autodate"
-  }))
+  if (!collection.fields.hasFieldWithName("updated")) {
+    collection.fields.addAt(2, new Field({
+      "hidden": false,
+      "id": "autodate3332085495",
+      "name": "updated",
+      "onCreate": true,
+      "onUpdate": true,
+      "presentable": false,
+      "system": false,
+      "type": "autodate"
+    }))
+  }
 
   return app.save(collection)
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_4269230398")
-
-  // remove field
-  collection.fields.removeById("autodate2990389176")
-
-  // remove field
-  collection.fields.removeById("autodate3332085495")
 
   return app.save(collection)
 })
